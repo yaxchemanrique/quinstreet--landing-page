42d73fbaa930b7dc5bdd901bf4d1d3f0035ccc11
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-11-27 13:33:13 -0800

    feat: :lipstick: add styles for .article border with JS for dynamic width
    Adds a complex border to the `.article` element using CSS linear gradients. The `index.js` file has been updated to dynamically capture the width of the `.article` element and set it as a custom property.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// ------------------------------
+// STYLING RELATED SETUP
+// ------------------------------
+
+const article = document.querySelector('.article');
+const articleTitle = article.querySelector('#article-title');
+article.style.setProperty('--_title-width', articleTitle.offsetWidth + 'px');
